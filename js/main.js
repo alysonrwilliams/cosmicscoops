d1b65b27b77d606104577e0015d9f40d42eb1047
Alyson Williams <alysonrwilliams@gmail.com>
2021-07-26 20:37:32 -0400

    added fetch function for NASA API

--- js/main.js
+++ js/main.js
@@ -23,8 +23,6 @@ const deserts = [
 }
 
 
-
-
 /* This function validates the name field + prompts alert to customer on submit */
 function myfunction() {
 var x=document.getElementById('fname').value;
@@ -36,8 +34,7 @@ alert("Thanks, " +x + "! Your ice cream is being prepared!");
 }
 }
 
-
-/* Tip Calculator Function*/
+/* This is the Tip Calculator Function*/
 function tipCalculator (slider, bill){
   var tip = document.getElementById('tipamount');
   var slideval = document.getElementById('slideval');
@@ -58,3 +55,33 @@ function tipCalculator (slider, bill){
   slideval.innerHTML = slider + '%';
   }
 }
+
+
+/* This is fetching the NASA API */
+
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const api_key = config.NASA_API_KEY
+
+const fetchNASAData = async () => {
+  try {
+    const response = await fetch(`${url}${api_key}`)
+    const data = await response.json()
+    console.log('NASA APOD data', data)
+    displayData(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/*This is displaying the NASA image on the site */
+
+const displayData = data => {
+  document.getElementById('title').textContent = data.title
+  document.getElementById('date').textContent = data.date
+  document.getElementById('picture').src = data.hdurl
+  document.getElementById('explanation').textContent = data.explanation
+}
+
+fetchNASAData()
+
+fetchNASAData()
